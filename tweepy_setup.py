b1f69614f07f4c1c32b7fd342ca0d0d1c5869d6f
Prince Yadav <143831813+iprinceyadav@users.noreply.github.com>
2024-10-06 21:52:17 +0530

    Update tweepy_setup.py

--- tweepy_setup.py
+++ tweepy_setup.py
@@ -7,8 +7,12 @@ access_token = "1579348101579608066-Q7DQeIa8gEWlxQeqZG4PgiQSrAGsvI"
 access_token_secret = "cy38ctZUftbQcYKAfUqwytYyfkwiQw5f5uO8NiorvRqcL"
 
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+#this will create a tweet in my account with this text 
+response1 = client.create_tweet(text="Hello Twitter!, this is auto generated")
+print(response)
 
+#this will delete the tweet of this id.
 tweet_id = 1842644817916125426
 
-response = client.delete_tweet(tweet_id)
+response2 = client.delete_tweet(tweet_id)
 print(response)
